11cc6ad41f80cf1f77fd7e5b35484da9bf7ba455
levolex@yandex.ru <levolex@yandex.ru@a8a411ea-fdc6-11e6-8e72-91c8148b5af0>
2022-04-24 20:53:24 +0000

    Use Win1251 for Accentuator no matter what is OS locale

--- qtextprocessor.h
+++ qtextprocessor.h
@@ -14,7 +14,9 @@ public:
   explicit QTextProcessor(QObject *parent = nullptr);
   static QString processByAccentuator(QString str);
   static QString fileToString(QString filepath, bool isUtf8 = true);
+  static QString fileWin1251ToString(QString filepath);
   static bool stringToFile(QString str, QString filepath, bool isUtf8 = true);
+  static bool stringToFileWin1251(QString str, QString filepath);
   static QStringList splitStringBySize(QString inStr, int size, QString headerDelimiter);
   static QStringList splitStringBySentences(QString inStr);
   static QString removeCDATA(QString& str);
